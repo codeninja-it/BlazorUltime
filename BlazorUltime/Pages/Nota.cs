12253e3df1a1e47f70b8cc2a408ebb40fb370109
Daniele Simoncini <simoncini@codeninja.it>
2021-04-09 13:04:09 +0200

    termine prima giornata di creazione servizio post-it online

---
+++ BlazorUltime/Pages/Nota.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUltime.Pages
+{
+	/// <summary>
+	/// Classe descrittiva per l'elemento di banca dati Nota
+	/// </summary>
+	public class Nota
+	{
+		/// <summary>
+		/// titolo della nota in formato string
+		/// </summary>
+		public string Titolo { get; set; }
+		/// <summary>
+		/// corpo della nota in formato string
+		/// </summary>
+		public string Corpo { get; set; }
+		/// <summary>
+		/// Giorno ed ora di creazione della nostra nota
+		/// </summary>
+		public DateTime Quando { get; set; }
+	}
+}
